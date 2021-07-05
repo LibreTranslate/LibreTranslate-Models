d391967b28e1b14b8db879357d29acd18d9d0b53
P.J. Finlay <pjappdevelopment@gmail.com>
2021-07-04 19:34:43 -0500

    Added argospm index

---
+++ argospm-index/format.sh
@@ -0,0 +1,2 @@
+python3 -m json.tool index.json > index-formatted.json
+mv index-formatted.json index.json
---
+++ argospm-index/web/script.js
@@ -0,0 +1,52 @@
+var packageIndex = "https://raw.githubusercontent.com/argosopentech/argospm-index/main/index.json";
+
+$(document).ready(function(){
+    var baseDiv = $("#argospm-index-table");
+    var argospmIndexTable = $("<table/>");
+    baseDiv.append(argospmIndexTable);
+    var heading = $("<tr/>");
+    heading.append("<th>From</th>");
+    heading.append("<th>To</th>");
+    heading.append("<th>Download</th>");
+    heading.append("<th>From code</th>");
+    heading.append("<th>To code</th>");
+    heading.append("<th>Package version</th>");
+    heading.append("<th>Argos version</th>");
+    heading.append("<th>IPFS</th>");
+    argospmIndexTable.append(heading);
+    $.ajax({url: packageIndex,
+	contentType: "",
+	success: function(response) {
+            var data = $.parseJSON(response);
+	    $.each(data, function(i, packageData) {
+		var tr = $("<tr>");
+		var fromName = $("<td/>").append(packageData.from_name);
+		tr.append(fromName);
+		var toName = $("<td/>").append(packageData.to_name);
+		tr.append(toName);
+		var linkTd = $("<td/>");
+		var link = $("<a/>").append("https");
+		link.attr("href", packageData.links[0]);
+		linkTd.append(link);
+		tr.append(linkTd);
+		var fromCode = $("<td/>").append(packageData.from_code);
+		tr.append(fromCode);
+		var toCode = $("<td/>").append(packageData.to_code);
+		tr.append(toCode);
+		var packageVersion = $("<td/>").append(packageData.package_version);
+		tr.append(packageVersion);
+		var argosVersion = $("<td/>").append(packageData.argos_version);
+		tr.append(argosVersion);
+		var ipfsLinkTd = $("<td/>");
+		if (packageData.links.length > 1){
+			var link = $("<a/>").append("IPFS");
+			link.attr("href", packageData.links[1]);
+			ipfsLinkTd.append(link);
+		}
+		tr.append(ipfsLinkTd);
+
+		argospmIndexTable.append(tr);
+	    });
+	}
+    });
+});
